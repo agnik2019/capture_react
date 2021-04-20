7f8620336b9324cdca7f9aaebc7a2e90f9a378a5
Agnik Saha <agniksaha2019@gmail.com>
2021-04-20 17:25:29 +0530

    4. Faq Section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+export default function FaqSection() {
+    return (
+        <div className="faq">
+            <h2>Any Questions <span>FAQ</span></h2>
+            <div className="questions">
+                <h4>How do I Start?</h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, porro.</p>
+                </div>
+            </div>
+
+            <div className="questions">
+                <h4>Daily schedule</h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, porro.</p>
+                </div>
+            </div>
+
+            <div className="questions">
+                <h4>Different Payment Methods</h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, porro.</p>
+                </div>
+            </div>
+
+            <div className="questions">
+                <h4>What products do you offer</h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, porro.</p>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/pages/Aboutus.js
+++ src/pages/Aboutus.js
@@ -1,11 +1,13 @@
 import AboutSection from '../components/AboutSection'
 import ServicesSection from '../components/ServicesSection';
+import FaqSection from "../components/FaqSection"
 
 const Aboutus = () => {
     return (
         <>
         <AboutSection />
         <ServicesSection />
+        <FaqSection/>
         </>
     )
     
