0ca48e8c472a5641fc4ea6cf9754933f120e1603
Agnik Saha <agniksaha2019@gmail.com>
2021-04-20 18:36:21 +0530

    7. styling faq section

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,12 +1,15 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import {About} from '../styles';
 
 export default function FaqSection() {
     return (
-        <div className="faq">
+        <Faq>
             <h2>Any Questions <span>FAQ</span></h2>
             <div className="questions">
                 <h4>How do I Start?</h4>
                 <div className="answer">
+                    <div className="faq-line"></div>
                     <p>Lorem ipsum dolor sit amet.</p>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, porro.</p>
                 </div>
@@ -15,6 +18,7 @@ export default function FaqSection() {
             <div className="questions">
                 <h4>Daily schedule</h4>
                 <div className="answer">
+                <div className="faq-line"></div>
                     <p>Lorem ipsum dolor sit amet.</p>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, porro.</p>
                 </div>
@@ -23,6 +27,7 @@ export default function FaqSection() {
             <div className="questions">
                 <h4>Different Payment Methods</h4>
                 <div className="answer">
+                <div className="faq-line"></div>
                     <p>Lorem ipsum dolor sit amet.</p>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, porro.</p>
                 </div>
@@ -31,10 +36,39 @@ export default function FaqSection() {
             <div className="questions">
                 <h4>What products do you offer</h4>
                 <div className="answer">
+                <div className="faq-line"></div>
                     <p>Lorem ipsum dolor sit amet.</p>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, porro.</p>
                 </div>
             </div>
-        </div>
+        </Faq>
     )
 }
+
+const Faq = styled(About)`
+    display:block;
+    span{
+        display:block;
+    }
+    h2{
+        padding-bottom: 2rem;
+        font-weight:lighter;
+    }
+    .faq-line{
+        background: #ccc;
+        height:0.2rem;
+        margin:2rem 0rem;
+        width:100%;
+    }
+    .question{
+        padding:3rem 0rem;
+        cursor:pointer;
+    }
+    .answer{
+        padding: 2rem 0rem;
+        p{
+            padding:1rem 0rem;
+        }
+    }
+`;
+
