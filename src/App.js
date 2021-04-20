290ac14c72b2dac0b6adec766aad1f4db7f86c52
Agnik Saha <agniksaha2019@gmail.com>
2021-04-20 17:58:48 +0530

    5. Intro To Styled Component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Aboutus from "./pages/Aboutus"
+import GlobalStyle from "./components/GlobalStyle"
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
      <Aboutus/>
     </div>
   );
